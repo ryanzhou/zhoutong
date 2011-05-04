0d6d6cd7753be0147b2cf267f32cb0095b99903f
Zhou Tong <ryan@xwaylab.com>
2011-05-04 15:58:04 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zhoutong::Application.config.secret_token = '8d833e8f6c580836d514186020770ce97bc64c53671482206c0c0f9e7ec17555cad973e917980f532447cc5225e7f9499d35e921e4187a7321f9dfb12a9d8e88'
